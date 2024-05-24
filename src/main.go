ca6d0baddd504c602b2bb089efbdcaec699e5013
D4ITON <beleghostdesarrollador@gmail.com>
2024-05-24 07:03:18 -0500

    se agrega declaracion de variables y constantes

--- src/main.go
+++ src/main.go
@@ -3,5 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola mundo")
+	// Declaracion de constantes
+	const pi float64 = 3.14
+	const pi2 = 3.15
+
+	fmt.Println("pi :", pi)
+	fmt.Println("pi2: ", pi2)
+
+	// Declaracion de variables
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println("base: ", base)
+	fmt.Println("altura: ", altura)
+	fmt.Println("area: ", area)
+
+	// valores por defecto si no los inicializamos (zero values)
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Area cuadrado
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println(areaCuadrado)
 }
