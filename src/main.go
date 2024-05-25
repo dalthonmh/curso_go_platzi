f561abed634a43658d8babab87177c6fa24c9d69
D4ITON <beleghostdesarrollador@gmail.com>
2024-05-25 12:59:09 -0500

    se agregan operadores aritmeticos

--- src/main.go
+++ src/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	// Declaracion de constantes
@@ -18,17 +21,76 @@ func main() {
 	fmt.Println("base: ", base)
 	fmt.Println("altura: ", altura)
 	fmt.Println("area: ", area)
+	fmt.Println("-----------------")
 
 	// valores por defecto si no los inicializamos (zero values)
-	var a int
-	var b float64
-	var c string
-	var d bool
+	// var a int
+	// var b float64
+	// var c string
+	// var d bool
 
-	fmt.Println(a, b, c, d)
+	// fmt.Println(a, b, c, d)
 
 	// Area cuadrado
-	const baseCuadrado = 10
-	areaCuadrado := baseCuadrado * baseCuadrado
-	fmt.Println(areaCuadrado)
+	// const baseCuadrado = 10
+	// areaCuadrado := baseCuadrado * baseCuadrado
+	// fmt.Println(areaCuadrado)
+
+	// operaciones aritmeticas
+	x := 10
+	y := 20
+	// valores iniciales
+	fmt.Println("Valor inicial de x: ", x)
+	fmt.Println("Valor inicial de y: ", y)
+
+	result := x + y
+
+	// suma
+	fmt.Println("suma: ", result)
+
+	// resta
+	result = y - x
+	fmt.Println("resta: ", result)
+
+	// multiplicacion
+	result = x * y
+	fmt.Println("producto: ", result)
+
+	// division
+	result = y / x
+	fmt.Println("division: ", result)
+
+	// incrementar
+	x++
+	fmt.Println("Incrementar x: ", x)
+
+	// decrementar
+	x--
+	fmt.Println("decrementar x: ", x)
+
+	fmt.Println("-----------------")
+	// cacular areas
+	// trapecio y circulo
+	trapecio_base_mayor := 10
+	trapecio_altura := 4
+	trapecio_base_menor := 8
+
+	fmt.Println("trapecio_base_mayor: ", trapecio_base_mayor)
+	fmt.Println("trapecio_base_menor: ", trapecio_base_menor)
+	fmt.Println("trapecio_altura: ", trapecio_altura)
+
+	// formula de area es A = (a+b)h/2
+	trapecio_area := (trapecio_base_mayor + trapecio_base_menor) * trapecio_altura / 2
+	fmt.Println("Area de trapecio: ", trapecio_area)
+
+	// area circulo
+	fmt.Println("-----------------")
+	circulo_radio := 4
+	// formula area de circulo: A = pi * r * r
+	fmt.Println("Radio de circulo: ", circulo_radio)
+
+	circulo_area := pi * float64(circulo_radio) * float64(circulo_radio)
+	fmt.Println("Area de circulo: ", circulo_area)
+	fmt.Println("El área del círculo es:", math.Pi*math.Pow(float64(circulo_radio), 2))
+
 }
