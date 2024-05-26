2b8d7bdfb1f7467e6689e4820fe08ba75ed89c86
D4ITON <beleghostdesarrollador@gmail.com>
2024-05-25 20:40:50 -0500

    uso de fmt para imprimir en consola
    fmt.Println, fmt.Printf, fmt.Sprintf

--- src/main.go
+++ src/main.go
@@ -1,96 +1,30 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 func main() {
-	// Declaracion de constantes
-	const pi float64 = 3.14
-	const pi2 = 3.15
+	// declaracion de variables
+	helloMessage := "Hola"
+	worldMessage := "mundo"
 
-	fmt.Println("pi :", pi)
-	fmt.Println("pi2: ", pi2)
+	// Println con fmt
+	fmt.Println(helloMessage, worldMessage)
 
-	// Declaracion de variables
-	base := 12
-	var altura int = 14
-	var area int
+	persona := "Dalthon"
+	actividad := "Curso de Go"
+	anio := 2024
+	// Donde: %s es para variables de tipo string y %d para variables de tipo int, %v es indistinto el tipo de dato
+	// Le pasamos los valores en orden
 
-	fmt.Println("base: ", base)
-	fmt.Println("altura: ", altura)
-	fmt.Println("area: ", area)
-	fmt.Println("-----------------")
+	// Printf
+	fmt.Printf("%s esta aprendiendo %s el anio %d\n", persona, actividad, anio)
 
-	// valores por defecto si no los inicializamos (zero values)
-	// var a int
-	// var b float64
-	// var c string
-	// var d bool
+	// Sprintf
+	message := fmt.Sprintf("%s esta aprendiendo %s el anio %d", persona, actividad, anio)
+	fmt.Println(message)
 
-	// fmt.Println(a, b, c, d)
-
-	// Area cuadrado
-	// const baseCuadrado = 10
-	// areaCuadrado := baseCuadrado * baseCuadrado
-	// fmt.Println(areaCuadrado)
-
-	// operaciones aritmeticas
-	x := 10
-	y := 20
-	// valores iniciales
-	fmt.Println("Valor inicial de x: ", x)
-	fmt.Println("Valor inicial de y: ", y)
-
-	result := x + y
-
-	// suma
-	fmt.Println("suma: ", result)
-
-	// resta
-	result = y - x
-	fmt.Println("resta: ", result)
-
-	// multiplicacion
-	result = x * y
-	fmt.Println("producto: ", result)
-
-	// division
-	result = y / x
-	fmt.Println("division: ", result)
-
-	// incrementar
-	x++
-	fmt.Println("Incrementar x: ", x)
-
-	// decrementar
-	x--
-	fmt.Println("decrementar x: ", x)
-
-	fmt.Println("-----------------")
-	// cacular areas
-	// trapecio y circulo
-	trapecio_base_mayor := 10
-	trapecio_altura := 4
-	trapecio_base_menor := 8
-
-	fmt.Println("trapecio_base_mayor: ", trapecio_base_mayor)
-	fmt.Println("trapecio_base_menor: ", trapecio_base_menor)
-	fmt.Println("trapecio_altura: ", trapecio_altura)
-
-	// formula de area es A = (a+b)h/2
-	trapecio_area := (trapecio_base_mayor + trapecio_base_menor) * trapecio_altura / 2
-	fmt.Println("Area de trapecio: ", trapecio_area)
-
-	// area circulo
-	fmt.Println("-----------------")
-	circulo_radio := 4
-	// formula area de circulo: A = pi * r * r
-	fmt.Println("Radio de circulo: ", circulo_radio)
-
-	circulo_area := pi * float64(circulo_radio) * float64(circulo_radio)
-	fmt.Println("Area de circulo: ", circulo_area)
-	fmt.Println("El área del círculo es:", math.Pi*math.Pow(float64(circulo_radio), 2))
+	// Para saber tipo de datos
+	fmt.Printf("Tipo de dato de 'persona': %T\n", persona)
+	fmt.Printf("Tipo de dato de 'anio': %T\n", anio)
 
 }
