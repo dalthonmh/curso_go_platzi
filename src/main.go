4b77108808ea4338968d04e35e798d69b88ed778
D4ITON <beleghostdesarrollador@gmail.com>
2024-05-27 08:04:13 -0500

    soluciario reto 1 y 2 de clase condicional if

--- src/main.go
+++ src/main.go
@@ -4,29 +4,57 @@ import (
 	"fmt"
 )
 
+func determinarParImpar(numero int) string {
+	if numero%2 == 0 {
+		return "Par"
+	}
+	return "Impar"
+}
+
+// Definimos una estructura de usuario
+type Usuario struct {
+	usuario  string
+	password string
+}
+
+// Definimos la funcion para saber si existe el usuario
+func login(usuario, password string) string {
+	// Creamos un array de 3 elementos de usuarios
+	var usuarios [3]Usuario
+
+	// Asignamos valores a los elementos del array
+	usuarios[0] = Usuario{usuario: "Dalthon", password: "123"}
+	usuarios[1] = Usuario{usuario: "Pepe", password: "abc"}
+	usuarios[2] = Usuario{usuario: "Jose", password: "xyz"}
+
+	// Retorna token de acceso
+	token := ""
+
+	for _, user := range usuarios {
+		if user.usuario == usuario && user.password == password {
+			token = "token-" + user.usuario
+		}
+	}
+
+	if token != "" {
+		return token
+	}
+
+	return "No existe el usuario"
+
+}
+
 func main() {
-	// ciclo for
-	for i := 0; i < 10; i++ {
-		fmt.Println("valor de i:", i)
-	}
+	// Reto1: Determinar si un número es par o impar
+	numero := 3
+	respuesta := determinarParImpar(numero)
+	fmt.Println("El número", numero, "es:", respuesta)
 
-	// ciclo for while
-	contador := 0
-	for contador < 10 {
-		fmt.Println("Valor for while contador:", contador)
-		contador++
-	}
+	// Reto2: Funcionalidad login
+	usuario := "Dalthon"
+	password := "123"
 
-	// ciclo for forever
-	// para terminar la ejecucion apreta control + c
-	// for {
-	// 	fmt.Println("Dalthon es un crack!")
-	// }
+	isLoginOk := login(usuario, password)
+	fmt.Println(isLoginOk)
 
-	// for range, similar a foreach en otros lenguajes
-	listaFrutas := []string{"pera", "manzana", "platano", "higo", "durazno"}
-
-	for index, fruta := range listaFrutas {
-		fmt.Printf("Fruta %d: %s\n", index, fruta)
-	}
 }
