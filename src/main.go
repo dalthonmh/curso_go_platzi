11b2aa39160ffeb737d6bf5f6255f1731b277080
D4ITON <beleghostdesarrollador@gmail.com>
2024-06-08 21:28:58 -0500

    se agrega info slicing

--- src/main.go
+++ src/main.go
@@ -5,22 +5,22 @@ import (
 )
 
 func main() {
-	// defer: sirve para ejecutar al ultimo de las instrucciones
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
+	// array y slices
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array)
+	fmt.Println(array, "tamanio array:", len(array), "capacidad array:", cap(array))
 
-	// Continue y break
-	for i := 0; i < 10; i++ {
-		//continue
-		if i == 2 {
-			fmt.Println("OJO: llegue a 2, continuo y no escribo el número de iteación")
-			continue
-		}
-		//break
-		if i == 8 {
-			fmt.Println("OJO: llegue a 8, rompo todo el bucle")
-			break
-		}
-		fmt.Println("iteración", i, "terminada")
-	}
+	// slices
+	// en los slices no va la cantidad de tamaño que va a tener el array
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, "tamanio slice:", len(slice), "capacidad slice:", cap(slice))
+
+	// slicing
+	// metodos en el slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
 }
