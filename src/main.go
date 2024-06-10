15f51b52b8a79ed592f4b0a580f9ac1fb1988e23
D4ITON <beleghostdesarrollador@gmail.com>
2024-06-10 05:39:00 -0500

    se agrega structs en golang

--- src/main.go
+++ src/main.go
@@ -4,37 +4,18 @@ import (
 	"fmt"
 )
 
+type auto struct {
+	marca string
+	anio  int
+}
+
 func main() {
-	peso_cohetes := make(map[string]int)
+	miauto := auto{marca: "toyota", anio: 2017}
+	fmt.Println(miauto)
 
-	peso_cohetes["starship"] = 5000000
-	peso_cohetes["falcon9"] = 549054
-	peso_cohetes["dragon"] = 4201
-	peso_cohetes["falconheavy"] = 1420788
+	// otra declaracion de auto
+	var segundo_auto auto
+	segundo_auto.marca = "tesla"
 
-	fmt.Println(peso_cohetes)
-
-	// recorrer valor
-	for i, v := range peso_cohetes {
-		fmt.Println(i, v)
-	}
-
-	// obtener un solo valor
-	// ok indica si la llave existe o no en el diccionario
-	value, ok := peso_cohetes["dragon"]
-	fmt.Println(value, ok)
-
-	// obtener un valor no existente
-	value2, ok := peso_cohetes["noexiste"]
-	fmt.Println(value2, ok)
-
-	// otra forma de declarar
-	//Declaration
-	var map_1 = map[string]int32{
-		"Car":      50000,
-		"House":    20000,
-		"Computer": 1000,
-	}
-
-	fmt.Println(map_1)
+	fmt.Println(segundo_auto)
 }
