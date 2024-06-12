afbbf384b9264debecec7d398a5a52f1e0f69314
dalthonmh <daitonmh100@gmail.com>
2024-06-12 16:22:58 -0500

    se agrega hola mundo desde local pc

--- src/main.go
+++ src/main.go
@@ -4,18 +4,6 @@ import (
 	"fmt"
 )
 
-type auto struct {
-	marca string
-	anio  int
-}
-
 func main() {
-	miauto := auto{marca: "toyota", anio: 2017}
-	fmt.Println(miauto)
-
-	// otra declaracion de auto
-	var segundo_auto auto
-	segundo_auto.marca = "tesla"
-
-	fmt.Println(segundo_auto)
+	fmt.Println("Hola mundo")
 }
