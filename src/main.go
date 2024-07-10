8b381b527885196e4dcc402cb5ff8ff0c1709d2e
D4ITON <beleghostdesarrollador@gmail.com>
2024-07-10 07:13:39 -0500

    se agregan chanels para concurrencia go

--- src/main.go
+++ src/main.go
@@ -1,29 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-	"time"
-)
+import "fmt"
 
-func say(text string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Println(text)
+func say(text string, c chan<- string) {
+	c <- text
 }
 
 func main() {
-	var wg sync.WaitGroup
+	c := make(chan string, 1)
+	fmt.Println("Hello")
 
-	fmt.Println("Hola")
-	wg.Add(1)
-	go say("world", &wg)
-
-	wg.Wait()
-
-	go func(text string) {
-		fmt.Println("Adios", text)
-	}("Dalthon")
-
-	time.Sleep(time.Second * 1)
+	go say("bye", c)
+	fmt.Println(<-c)
 
 }
